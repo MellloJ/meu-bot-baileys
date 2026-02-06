80ea028f06d4b6de59e3330ef5b43ca2bcab221c
MellloJ <jdesousamadeira@gmail.com>
2026-02-06 19:57:27 -0300

    Tentando corrigir o comando de busca e download de musicas e letras

--- src/settings/youtubeAuth.js
+++ src/settings/youtubeAuth.js
@@ -8,7 +8,7 @@ async function initYoutubeAuth() {
     if (process.env.YOUTUBE_COOKIE) {
         await play.setToken({
             youtube: {
-                cookie: cookies,
+                cookie: process.env.YOUTUBE_COOKIE,
                 userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0"
             }
         });
