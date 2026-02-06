341abadc6cb963f583f2e6926872c84c377f0662
MellloJ <jdesousamadeira@gmail.com>
2026-02-06 13:53:06 -0300

    Tentando corrigir o erro de undefined

--- src/commands/index.js
+++ src/commands/index.js
@@ -7,8 +7,8 @@ module.exports = {
     add: require('./AddCommand'),
     play: require('./PlayCommand'),
     musica: require('./PlayCommand'), // Alias
-    s: require('./StickerCommand'),
-    setup: require('./SetupCommand'),
+    // s: require('./StickerCommand'),
+    // setup: require('./SetupCommand'),
     hidetag: require('./HidetagCommand'),
     help: require('./HelpCommand'),
     dono: require('./DonoCommand'),
